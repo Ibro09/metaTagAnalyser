cc508c24c73d3398e092c745773e024e2f8c27c8
ibrahim <99294496+Ibro09@users.noreply.github.com>
2023-10-02 13:28:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ app.get("/analyze", async (req, res) => {
     res.json({ imageAltTexts });
   } catch (error) {
     console.error("Error:", error);
-    res.status(500).json({ error: "An error occurred" });
+    res.status(500).json({ error: error });
   }
 });
 
